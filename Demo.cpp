7e16e74cd8ed6bec3f4dcb29eb46f9b4ac596613
Steve Sanbeg <steve.sanbeg@gmail.com>
2021-09-24 19:25:04 -0400

    improve motion handling, create test module

--- Demo.cpp
+++ Demo.cpp
@@ -1,19 +1,20 @@
 
 #include "Demo.h"
 #include "Control.h"
-#include "font6x8AJ2.h"
+#include "oled_text.h"
 
 void Demo::setup() {
-  
-  ssd1306_char_f6x8(0, 1, "Hello World.");
-  ssd1306_char_f6x8(0, 2, "hacked i2c lib");
+    using oled_text::ssd1306_char_f6x8;
+
+  ssd1306_char_f6x8(oled, 0, 1, "Hello World.");
+  ssd1306_char_f6x8(oled, 0, 2, "hacked i2c lib");
 }
 
 void Demo::loop() {
-  
+  using oled_text::ssd1306_char_f6x8;
   char btn_text[7];
   control::debug(btn_text);
-  ssd1306_char_f6x8(0, 5, btn_text);
+  ssd1306_char_f6x8(oled, 0, 5, btn_text);
 
   oled.ssd1306_setpos(dotx, doty);
   oled.ssd1306_send_data_start();
@@ -49,31 +50,4 @@ void Demo::loop() {
   oled.ssd1306_send_data_stop();
 
   //delay(128);
-}
-
-
-// move to a text class?
-void Demo::ssd1306_char_f6x8(uint8_t x, uint8_t y, const char ch[]) {
-  uint8_t c, i, j = 0;
-  while (ch[j] != '\0')
-  {
-    c = ch[j] - 32;
-    if (c > 0) c = c - 12;
-    if (c > 15) c = c - 6;
-    if (c > 40) c = c - 9;
-    if (x > 126)
-    {
-      x = 0;
-      y++;
-    }
-    oled.ssd1306_setpos(x, y);
-    oled.ssd1306_send_data_start();
-    for (i = 0; i < 6; i++)
-    {
-      oled.ssd1306_send_byte(pgm_read_byte(&ssd1306xled_font6x8[c * 6 + i]));
-    }
-    oled.ssd1306_send_data_stop();
-    x += 6;
-    j++;
-  }
 }
---
+++ oled_text.h
@@ -0,0 +1,9 @@
+
+#include<stdint.h>
+#include "ssd1306xled.h"
+
+namespace oled_text {
+
+void ssd1306_char_f6x8(SSD1306Device oled, uint8_t x, uint8_t y, const char ch[]);
+  
+}
