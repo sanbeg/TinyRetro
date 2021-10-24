784b54b8b81f7d53d352f8da4cc47db69a8eea3c
Steve Sanbeg <steve.sanbeg@gmail.com>
2021-10-23 23:25:31 -0400

    optimize sound, save about 28 bytes

--- TinyRetro/Beeper.h
+++ TinyRetro/Beeper.h
@@ -1,26 +1,29 @@
 
+#include <avr/io.h>
+
+
 template <int P=PB0>
 class Beeper
 {
   private:
-  const uint8_t mask = 1<<P;
+  static const uint8_t mask = 1<<P;
   public:
-    boolean mute = 0;         // Mute the speaker
+    bool mute = 0;         // Mute the speaker
 
     void setup() {
-      //pinMode(P, OUTPUT);
+      //pinMode(10, OUTPUT);
       DDRB |= mask;
     }
 
     void beep(int bCount, int bDelay) {
       if (mute) return;
       for (int i = 0; i <= bCount; i++) {
-        //digitalWrite(P, HIGH);
+        //digitalWrite(10, HIGH);
         PORTB |= mask;
         for (int i2 = 0; i2 < bDelay; i2++) {
           __asm__("nop\n\t");
         }
-        //digitalWrite(P, LOW);
+        //digitalWrite(10, LOW);
         PORTB &= ~mask;
         for (int i2 = 0; i2 < bDelay; i2++) {
           __asm__("nop\n\t");
