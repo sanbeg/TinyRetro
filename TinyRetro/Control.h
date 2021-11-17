a62860077f66845625cacde1acbc0ef2d10a09f5
Steve Sanbeg <steve.sanbeg@gmail.com>
2021-11-16 19:14:09 -0500

    change button order to make it easier to route pcb

--- TinyRetro/Control.h
+++ TinyRetro/Control.h
@@ -6,12 +6,20 @@
 namespace control {
 
 enum ButtonPins {
+  /*
   BTN_L = 0,
   BTN_U = 1,
   BTN_R = 2,
   BTN_A = 3,
   BTN_B = 5,
   BTN_D = 7,
+  */
+  BTN_D = 7,
+  BTN_L = 5,
+  BTN_U = 3,
+  BTN_R = 2,
+  BTN_A = 1,
+  BTN_B = 0,
 };
 
 void setup();
